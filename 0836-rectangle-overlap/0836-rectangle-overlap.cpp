ec6431288b342b1372f576f264b7d6e6af6b2e38
Arbaj Ahmad <arbajahmadIT3333@gmail.com>
2026-09-14 21:18:04 +0530

    Time: 0 ms (100%), Space: 10 MB (0%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rect1, vector<int>& rect2) {
+
+        if (rect1[0] == rect1[2] || 
+            rect1[1] == rect1[3] || 
+            rect2[0] == rect2[2] ||
+            rect2[1] == rect2[3]) {
+            // the line cannot have positive overlap
+            return false;
+        }
+        return !(rect1[2] <= rect2[0] || rect1[3] <= rect2[1] ||
+                 rect1[0] >= rect2[2] || rect1[1] >= rect2[3]);
+    }
+};
